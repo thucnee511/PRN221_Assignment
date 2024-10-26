e0b94bee441920966d54e368ed48395777b1d8c5
neru <ngocngocthuc@gmail.com>
2024-10-26 17:13:56 +0700

    style: main window style

--- SE171089_WPF/MainWindow.xaml.cs
+++ SE171089_WPF/MainWindow.xaml.cs
@@ -17,9 +17,68 @@ namespace SE171089_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Button activeButton;
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void ActiveButton(object button)
+        {
+            Button clickedButton = (Button) button;
+            if (activeButton != null)
+            {
+                activeButton.Background = Brushes.AliceBlue;
+                activeButton.BorderBrush = Brushes.AliceBlue;
+                activeButton.Foreground = Brushes.Black;
+            }
+            clickedButton.Background = Brushes.White;
+            clickedButton.BorderBrush = Brushes.White;
+            clickedButton.Foreground = Brushes.Black;
+            activeButton = clickedButton;
+        }
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button != activeButton)
+            {
+                button.Background = Brushes.White;
+                button.BorderBrush = Brushes.White;
+                button.Foreground = Brushes.Black;
+            }
+        }
+        private void Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button != activeButton)
+            {
+                button.Background = Brushes.AliceBlue;
+                button.BorderBrush = Brushes.AliceBlue;
+                button.Foreground = Brushes.Black;
+            }
+        }
+        private void btnUser_Click(object sender, RoutedEventArgs e)
+        {
+            ActiveButton(sender);
+        }
+
+        private void btnCategory_Click(object sender, RoutedEventArgs e)
+        {
+            ActiveButton(sender);
+        }
+
+        private void btnBook_Click(object sender, RoutedEventArgs e)
+        {
+            ActiveButton(sender);
+        }
+
+        private void btnRent_Click(object sender, RoutedEventArgs e)
+        {
+            ActiveButton(sender);
+        }
+
+        private void btnQuit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
